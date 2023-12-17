1ab58792d50c9cb758676b4fe92494605c5092d3
laxmanpokhrel <laxmanpokhrel102@gmail.com>
2023-12-17 13:51:52 +0545

    chore: toolkit dependencies

---
+++ configurations/.store/.redux-toolkit/index.redux.toolkit.ts
@@ -0,0 +1,6 @@
+const dependencies: Record<string, any>[] = [
+  { name: "@reduxjs/toolkit", version: "^1.9.3" },
+];
+
+// Typescript dependencies if required any
+const typescriptDependencies: string[] = [""];
--- configurations/.store/index.ts
+++
@@ -1,6 +0,0 @@
-const dependencies: Record<string, any>[] = [
-  { name: "@reduxjs/toolkit", version: "^1.9.3" },
-];
-
-// Typescript dependencies if required any
-const typescriptDependencies: string[] = [""];
